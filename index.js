3aea4056c24688bcb73b355152c83f3a513569fa
maxkoryukov <maxkoryukov@yandex.ru>
2017-01-01 17:31:49 +0500

    Update LICENCE and *-ignore files

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/**
+ * Module dependencies.
+ */
+const Strategy = require('./src/strategy')
+
+/**
+ * Expose `Strategy` directly from package.
+ */
+exports = module.exports = Strategy
+
+/**
+ * Export constructors.
+ */
+exports.Strategy = Strategy
